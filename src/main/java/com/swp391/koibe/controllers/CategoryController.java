b08bc39df71e6849a33f44ecbee7370873c7e666
lcaohoanq <hoangclw@gmail.com>
2024-09-30 10:57:03 +0700

    feat: implemtn CategoryController with i18n

--- src/main/java/com/swp391/koibe/controllers/CategoryController.java
+++ src/main/java/com/swp391/koibe/controllers/CategoryController.java
@@ -1,10 +1,16 @@
 package com.swp391.koibe.controllers;
 
+import com.swp391.koibe.components.LocalizationUtils;
 import com.swp391.koibe.dtos.CategoryDTO;
+import com.swp391.koibe.exceptions.MethodArgumentNotValidException;
+import com.swp391.koibe.exceptions.base.DataNotFoundException;
+import com.swp391.koibe.responses.CategoryResponse;
 import com.swp391.koibe.services.category.ICategoryService;
 import jakarta.validation.Valid;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
@@ -20,68 +26,106 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("${api.prefix}/categories")
 @RequiredArgsConstructor
+@Slf4j
 public class CategoryController {
 
     private final ICategoryService categoryService;
+    private final LocalizationUtils localizationUtils;
 
     @GetMapping("")
-    public ResponseEntity<?> getAllCategories() {
-        try{
-            return ResponseEntity.ok(categoryService.getAllCategories());
-        }catch (Exception e){
-            return ResponseEntity.badRequest().body(e.getMessage());
+    public ResponseEntity<CategoryResponse> getAllCategories() {
+        CategoryResponse response = CategoryResponse.builder()
+            .message(localizationUtils.getLocalizedMessage("category.get_all_successfully"))
+            .listData(categoryService.getAllCategories())
+            .build();
+        try {
+            return ResponseEntity.ok(response);
+        } catch (Exception e) {
+            throw new DataNotFoundException(
+                localizationUtils.getLocalizedMessage("category.get_all_failed"));
         }
 
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<?> getCategoryById(@PathVariable int id) {
-        try{
-            return ResponseEntity.ok(categoryService.getById(id));
-        }catch (Exception e){
-            return ResponseEntity.badRequest().body(e.getMessage());
+    public ResponseEntity<CategoryResponse> getCategoryById(@PathVariable int id) {
+
+        try {
+            CategoryResponse response = CategoryResponse.builder()
+                .message(localizationUtils.getLocalizedMessage("category.get_by_id_successfully"))
+                .singleData(categoryService.getById(id))
+                .build();
+            return ResponseEntity.ok(response);
+        } catch (Exception e) {
+            throw new DataNotFoundException(
+                localizationUtils.getLocalizedMessage("category.get_by_id_failed"));
         }
     }
 
     @PostMapping("")
-    public ResponseEntity<?> createCategory(
+    public ResponseEntity<CategoryResponse> createCategory(
         @Valid @RequestBody CategoryDTO categoryDTO,
         BindingResult result) {
 
-        if(result.hasErrors()){
-            List<FieldError> fieldErrorList = result.getFieldErrors();
-            List<String> errorMessages = fieldErrorList
-                .stream()
-                .map(FieldError::getDefaultMessage)
-                .toList();
-            return ResponseEntity.badRequest().body(errorMessages);
+        if (result.hasErrors()) {
+            throw new MethodArgumentNotValidException(result);
         }
 
-        try{
+        try {
+            CategoryResponse response = CategoryResponse.builder()
+                .message(localizationUtils.getLocalizedMessage(
+                    "category.create_category.create_successfully"))
+                .singleData(categoryService.createCategory(categoryDTO))
+                .build();
+
             categoryService.createCategory(categoryDTO);
-            return ResponseEntity.status(201).body("Category created successfully");
-        }catch (Exception e){
-            return ResponseEntity.badRequest().body(e.getMessage());
+            return ResponseEntity.status(HttpStatus.CREATED).body(response);
+        } catch (Exception e) {
+            throw new RuntimeException(
+                localizationUtils.
+                    getLocalizedMessage("category.create_category.create_failed"));
         }
     }
 
     @PutMapping("/{id}")
-    public ResponseEntity<?> updateCategory(@PathVariable int id, @RequestBody CategoryDTO categoryDTO) {
-        try{
-            categoryService.update(id, categoryDTO);
-            return ResponseEntity.ok().body("Category updated successfully");
-        }catch (Exception e){
-            return ResponseEntity.badRequest().body(e.getMessage());
+    public ResponseEntity<CategoryResponse> updateCategory(
+        @PathVariable int id,
+        @Valid @RequestBody CategoryDTO categoryDTO,
+        BindingResult result) {
+
+        if (result.hasErrors()) {
+            throw new MethodArgumentNotValidException(result);
+        }
+
+        try {
+            CategoryResponse response = CategoryResponse.builder()
+                .message(localizationUtils.getLocalizedMessage(
+                    "category.update_category.update_successfully"))
+                .singleData(categoryService.update(id, categoryDTO))
+                .build();
+
+            return ResponseEntity.ok().body(response);
+        } catch (Exception e) {
+            throw new RuntimeException(
+                localizationUtils.
+                    getLocalizedMessage("category.update_category.update_failed"));
         }
     }
 
     @DeleteMapping("/{id}")
-    public ResponseEntity<?> deleteCategory(@PathVariable int id) {
-        try{
+    public ResponseEntity<CategoryResponse> deleteCategory(@PathVariable int id) {
+        try {
+           CategoryResponse response = CategoryResponse.builder()
+                .message(localizationUtils.getLocalizedMessage(
+                    "category.delete_category.delete_successfully", id))
+                .build();
+
             categoryService.delete(id);
-            return ResponseEntity.noContent().build();
-        }catch (Exception e){
-            return ResponseEntity.badRequest().body(e.getMessage());
+            return ResponseEntity.ok(response);
+        } catch (Exception e) {
+            throw new RuntimeException(
+                localizationUtils.
+                    getLocalizedMessage("category.delete_category.delete_failed"));
         }
     }
 
