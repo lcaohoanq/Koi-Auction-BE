920ece6894a23764937cfa4d97963675e8f3ceda
lcaohoanq <hoangclw@gmail.com>
2024-09-30 10:32:47 +0700

    feat: add RoleServiceTest

--- src/test/java/com/swp391/koibe/KoiBeApplicationTests.java
+++ src/test/java/com/swp391/koibe/KoiBeApplicationTests.java
@@ -2,6 +2,7 @@ package com.swp391.koibe;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
 class KoiBeApplicationTests {
