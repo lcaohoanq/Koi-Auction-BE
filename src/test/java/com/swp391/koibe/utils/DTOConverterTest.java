5d401bd67fd12f8a711fe323b668bace6115d3e7
lcaohoanq <hoangclw@gmail.com>
2024-10-03 20:37:02 +0700

    feat: add unittest for utils

---
+++ src/test/java/com/swp391/koibe/utils/DTOConverterTest.java
@@ -0,0 +1,35 @@
+package com.swp391.koibe.utils;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@SpringBootTest
+class DTOConverterTest {
+
+    @Test
+    void convertToUserDTO() {
+    }
+
+    @Test
+    void convertToKoiDTO() {
+    }
+
+    @Test
+    void convertToKoiImageDTO() {
+    }
+
+    @Test
+    void convertToAuctionDTO() {
+    }
+
+    @Test
+    void convertToAuctionKoiDTO() {
+    }
+
+    @Test
+    void convertToBidDTO() {
+    }
+}
---
+++ src/test/java/com/swp391/koibe/utils/DateTimeUtilsTest.java
@@ -0,0 +1,57 @@
+package com.swp391.koibe.utils;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import com.swp391.koibe.exceptions.MalformDataException;
+import com.swp391.koibe.models.Auction;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class DateTimeUtilsTest {
+
+    Auction existingAuction;
+    DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss.SSSSSS");
+
+    @BeforeEach
+    void setup() {
+        existingAuction = Auction.builder()
+            .startTime(LocalDateTime.parse("2024-10-03 12:00:00.000000", formatter))
+            .endTime(LocalDateTime.parse("2024-10-15 12:00:00.000000", formatter))
+            .build();
+    }
+
+    @Test
+    void isAuctionActive() {
+        LocalDateTime bidTime = LocalDateTime.parse("2024-10-10 12:00:00.000000", formatter);
+        assertTrue(DateTimeUtils.isAuctionActive(existingAuction.getStartTime(), existingAuction.getEndTime(), bidTime));
+    }
+
+    @Test
+    void parseTime() {
+        LocalDateTime expected = LocalDateTime.parse("2024-10-03 12:00:00.000000", formatter);
+        assertEquals(expected, DateTimeUtils.parseTime("2024-10-03 12:00:00.000000"));
+    }
+
+    @Test
+    void validateAuctionTimes() {
+        assertThrows(MalformDataException.class, () -> {
+            DateTimeUtils.validateAuctionTimes(
+                existingAuction.getStartTime(),
+                LocalDateTime.parse("2012-10-05 12:00:00.000000", formatter));
+        });
+
+        assertThrows(MalformDataException.class, () -> {
+            DateTimeUtils.validateAuctionTimes(
+                LocalDateTime.parse("2023-10-04 12:00:00.000000", formatter),
+                existingAuction.getEndTime());
+        });
+
+        assertThrows(MalformDataException.class, () -> {
+            DateTimeUtils.validateAuctionTimes(
+                existingAuction.getStartTime(),
+                LocalDateTime.parse("2024-10-02 12:00:00.000000", formatter));
+        });
+    }
+}
