e83d0be5c4ae2532247b6a822a81a51ddda5d844
lcaohoanq <hoangclw@gmail.com>
2024-10-25 15:22:35 +0700

    test: config run test with h2

--- src/test/java/com/swp391/koibe/utils/DateTimeUtilsTest.java
+++ src/test/java/com/swp391/koibe/utils/DateTimeUtilsTest.java
@@ -8,7 +8,11 @@ import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
+@SpringBootTest
+@ActiveProfiles("test")
 class DateTimeUtilsTest {
 
     Auction existingAuction;
