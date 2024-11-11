fdd7daaa0a88fd5fd88e849fe6db3142ab5492a9
lcaohoanq <hoangclw@gmail.com>
2024-11-11 14:15:13 +0700

    fix: validate ceiling price on asc bid

--- src/main/java/com/swp391/koibe/services/auctionkoi/AuctionKoiService.java
+++ src/main/java/com/swp391/koibe/services/auctionkoi/AuctionKoiService.java
@@ -89,9 +89,10 @@ public class AuctionKoiService implements IAuctionKoiService {
 
         // Validate ceil price based on bid method
         Long validCeilPrice = null;
-        if (EBidMethod.valueOf(auctionKoiDTO.bidMethod()) == EBidMethod.DESCENDING_BID) {
+        if (EBidMethod.valueOf(auctionKoiDTO.bidMethod()) == EBidMethod.DESCENDING_BID ||
+            EBidMethod.valueOf(auctionKoiDTO.bidMethod()) == EBidMethod.ASCENDING_BID) {
             if (auctionKoiDTO.ceilPrice() == null) {
-                throw new MalformDataException("Ceil price is required for DESCENDING_BID");
+                throw new MalformDataException("Ceil price is required for DESCENDING_BID and ASCENDING_BID");
             }
             validCeilPrice = auctionKoiDTO.ceilPrice(); // Set the provided ceil price
 
