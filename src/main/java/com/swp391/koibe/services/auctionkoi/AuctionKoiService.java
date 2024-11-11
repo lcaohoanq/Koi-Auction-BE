713d08af8b1559789d8ef9631c4358c0ff8fec77
lcaohoanq <hoangclw@gmail.com>
2024-11-11 20:20:43 +0700

    fix: fix auctionkoi

--- src/main/java/com/swp391/koibe/services/auctionkoi/AuctionKoiService.java
+++ src/main/java/com/swp391/koibe/services/auctionkoi/AuctionKoiService.java
@@ -48,11 +48,12 @@ public class AuctionKoiService implements IAuctionKoiService {
 
         // check if the koi is already in another upcoming auction
         Set<Auction> currentAuctionList = auctionRepository.findAuctionsByStatus(EAuctionStatus.UPCOMING);
+        currentAuctionList.addAll(auctionRepository.findAuctionsByStatus(EAuctionStatus.ONGOING));
         for (Auction auction : currentAuctionList) {
             List<AuctionKoi> auctionKoiList = auctionKoiRepository.findAuctionKoiByAuctionId(auction.getId());
             for (AuctionKoi auctionKoi : auctionKoiList) {
                 if (Objects.equals(auctionKoi.getKoi().getId(), auctionKoiDTO.koiId())) {
-                    throw new MalformDataException("This koi is already in another upcoming auction");
+                    throw new MalformDataException("This koi is already in another auction");
                 }
             }
         }
