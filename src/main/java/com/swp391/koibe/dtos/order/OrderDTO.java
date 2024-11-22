dde34143c4583bde6949b988b92089da5cb06b9b
lcaohoanq <hoangclw@gmail.com>
2024-11-22 11:48:04 +0700

    fix: update validate order phonenumber

--- src/main/java/com/swp391/koibe/dtos/order/OrderDTO.java
+++ src/main/java/com/swp391/koibe/dtos/order/OrderDTO.java
@@ -5,6 +5,7 @@ import com.swp391.koibe.dtos.CartItemDTO;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 import java.time.LocalDate;
 import java.util.List;
@@ -36,7 +37,7 @@ public class OrderDTO {
 
     @JsonProperty("phone_number")
     @NotBlank(message = "Phone number is required")
-    @Size(min = 5, message = "Phone number must be at least 5 characters")
+    @Pattern(regexp = "^(?:\\+84|0084|0)[235789][0-9]{1,2}[0-9]{7}$" , message = "Phone number must be a valid phone number")
     String phoneNumber;
 
     @JsonProperty("note")
